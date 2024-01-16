3cb7cbe71992ab19cebb77894b25d05b99b00612
robee05 <rbullonperez@safareyes.es>
2024-01-16 21:25:17 +0100

    landing page

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,32 +1,43 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {CarouselModule} from "ngx-bootstrap/carousel";
-
+import {GALLERY_CONFIG, GalleryComponent, GalleryConfig, GalleryItem, ImageItem} from 'ng-gallery';
+import {AppComponent} from "../app.component";
+import {bootstrapApplication} from "@angular/platform-browser";
+bootstrapApplication(AppComponent, {
+  providers: [
+    {
+      provide: GALLERY_CONFIG,
+      useValue: {
+        autoHeight: true,
+        imageSize: 'cover'
+      } as GalleryConfig
+    }
+  ]
+})
 @Component({
   selector: 'app-landingpage',
   standalone: true,
   imports: [
     RouterLink,
-    CarouselModule
+    CarouselModule,
+    GalleryComponent
   ],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css'
 })
 export class LandingpageComponent implements OnInit{
-  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+  images:any[];
+  constructor() {
+    this.images = [
+      new ImageItem({src:'assets/images/fotocarrusle.jpg', thumb: 'assets/images/fotocarrusle.jpg'}),
+      new ImageItem({src:'assets/images/fotocarrusle.jpg', thumb: 'assets/images/fotocarrusle.jpg'}),
+      new ImageItem({src:'assets/images/1.png', thumb: 'assets/images/1.png'}),
 
-  constructor() { }
+    ]
 
-  ngOnInit(): void {
-    this.slides[0] = {
-      src: './assets/img/angular.jpg',
-    };
-    this.slides[1] = {
-      src: './assets/img/react.jpg',
-    }
-    this.slides[2] = {
-      src: './assets/img/vue.jpg',
-    }
   }
+
+  ngOnInit(): void {}
 
 }

8dab5aff66135d0d8f308f86df7efdca2a37bac9
sgarcia <sgarciaguerrero@safareyes.es>
2024-01-19 20:16:05 +0100

    NO FUNCIONA EL COMPONENTE CARDS de ionic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {IndexComponent} from "./index/index.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: '', redirectTo: '/index', pathMatch: 'full'},
+  {path: 'index', component: IndexComponent},
+
+
+
+];

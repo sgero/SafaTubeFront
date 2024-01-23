212f7a710d8b9098f7275af00430e5835fa8528a
j20hook <jgarciamartin@safareyes.es>
2024-01-23 21:01:03 +0100

    componentes para notificacion

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {CampanaComponent} from "../campana/campana.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    CampanaComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'

aafcb7407974fdc33be5a76bc36de8d1501aadf4
robee05 <rbullonperez@safareyes.es>
2024-01-23 20:56:32 +0100

    home en proceso

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+    imports: [
+        RouterLink,
+        HeaderComponent
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

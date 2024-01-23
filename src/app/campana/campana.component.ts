68ea32e09497b7f40ab9902a8953c6ad2a1998a1
j20hook <jgarciamartin@safareyes.es>
2024-01-23 20:57:03 +0100

    componentes para notificacion

--- src/app/campana/campana.component.ts
+++ src/app/campana/campana.component.ts
@@ -2,11 +2,12 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Router, RouterLink} from "@angular/router";
 import {LikealertaComponent} from "../likealerta/likealerta.component";
 import {SuscripcionalertaComponent} from "../suscripcionalerta/suscripcionalerta.component";
+import {MensajealertaComponent} from "../mensajealerta/mensajealerta.component";
 
 @Component({
   selector: 'app-campana',
   standalone: true,
-  imports: [RouterLink, LikealertaComponent, SuscripcionalertaComponent],
+  imports: [RouterLink, LikealertaComponent, SuscripcionalertaComponent, MensajealertaComponent],
   templateUrl: './campana.component.html',
   styleUrl: './campana.component.css'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
 
+
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'

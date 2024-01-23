f5f2a0159ba644576058dd9cc815dea7161e1687
robee05 <rbullonperez@safareyes.es>
2024-01-23 17:35:10 +0100

    login prueba registro prueba

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import {LandingpageComponent} from "./landingpage/landingpage.component";
+import {RegisterComponent} from "./register/register.component";
+import {LoginComponent} from "./login/login.component";
 
 export const routes: Routes = [
   {path:'safaTube', component:LandingpageComponent},
   {path:'', redirectTo:'/safaTube', pathMatch:"full"},
+  {path:'safaTube/registro', component:RegisterComponent},
+  {path:'safaTube/login', component:LoginComponent},
 
 ];

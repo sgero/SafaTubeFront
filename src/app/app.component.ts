cc3c195ba7f6e60c9365b4af256b727d2ccbc862
robee05 <rbullonperez@safareyes.es>
2024-01-25 15:25:54 +0100

    home en proceso, 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {FooterComponent} from "./footer/footer.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 
 @Component({

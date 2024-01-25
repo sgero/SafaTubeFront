cf93d6cd3b8b4652405fff7f5dae5e0503102553
robee05 <rbullonperez@safareyes.es>
2024-01-25 20:47:36 +0100

    service

--- src/app/service/generalservice.ts
+++ src/app/service/generalservice.ts
@@ -1,14 +1,15 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+// import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Video} from "../models/Video";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class Generalservice {
-  constructor(private http: HttpClient) { }
-  enviarIdVideoPlayingBaseDatos(id:number){
-    return this.http.post<Video>('http://localhost:8080/api/video/get/', id)
-  }
+  // constructor(private http: HttpClient) { }
+  // enviarIdVideoPlayingBaseDatos(id:number){
+  //   return this.http.post<Video>('http://localhost:8080/api/video/get/', id)
+  // }
 }

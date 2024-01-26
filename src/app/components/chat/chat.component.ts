dd5e3c636eaf6a3ff81d84abbc06675aba1f4991
j20hook <jgarciamartin@safareyes.es>
2024-01-26 19:28:42 +0100

    comits que arreglan cosas

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -2,22 +2,27 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
 import {Router} from "@angular/router";
 import {Generalservice} from "../../service/generalservice";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-chat',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    FormsModule
   ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
-  // mensaje = {texto: '',usuarioEmisor: 1,usuarioReceptor:2}
+  mensaje = {texto: '', usuario_emisor:1,usuario_receptor:2}
   constructor(private service:Generalservice, private router: Router) {
 
   }
-  // crearMensaje(){
-  //   this.service.crearMensaje(this.mensaje).subscribe()
-  // }
+  crearMensaje(){
+    this.service.
+    crearMensaje(this.mensaje).subscribe(data => {
+      console.log(data);
+    })
+  }
 }
--- src/app/service/generalservice.ts
+++ src/app/service/generalservice.ts
@@ -5,17 +5,17 @@ import {Video} from "../models/Video";
 import {Mensaje} from "../models/Mensaje";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Generalservice {
 
   private url = 'http://localhost:8000';
-  constructor(private http: HttpClient, private httpmodule:HttpClientModule) { }
+  constructor(private http: HttpClient) { }
   enviarIdVideoPlayingBaseDatos(id:number){
     return this.http.post<Video>('http://localhost:8080/api/video/get/', id)
   }
 
   crearMensaje(data: Mensaje){
-    return this.http.post<Mensaje>(this.url + "/mensaje/crear", data);
+    return this.http.post<object>(this.url + "/api/mensaje/crear", data);
   }
 }

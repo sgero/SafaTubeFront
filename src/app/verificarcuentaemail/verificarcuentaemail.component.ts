23d0da792cd0f0fba9a25836da3d7b80d7b4ea53
sgarcia <sgarciaguerrero@safareyes.es>
2024-02-22 16:23:54 +0100

    puliendo codigo

--- src/app/verificarcuentaemail/verificarcuentaemail.component.ts
+++ src/app/verificarcuentaemail/verificarcuentaemail.component.ts
@@ -17,55 +17,7 @@ import {Usuario} from "../models/Usuario";
 })
 export class VerificarcuentaemailComponent {
 
-  //implements OnInit
 
-  // verificarEmailForm: FormGroup;
-  //
-  // constructor(
-  //   private fb: FormBuilder,
-  //   private generalService: Generalservice,
-  //   private router: Router,
-  //   private route: ActivatedRoute // 1. Importa ActivatedRoute
-  // ) {
-  //   this.verificarEmailForm = this.fb.group({
-  //
-  //     token: [''],
-  //   });
-  // }
-
-  // ngOnInit(): void {
-  //   this.route.queryParams.subscribe(params => { // 3. Obtén los parámetros de la URL
-  //     if ( params['token']) {
-  //       this.verificarEmailForm.patchValue({
-  //
-  //         token: params['token']
-  //       });
-  //       // Realiza la verificación automáticamente
-  //       this.verificarEmailUser();
-  //     }
-  //   });
-  // }
-  //
-  // // Método para verificar el usuario al comprobar que coincide el token de validación
-  // verificarEmailUser() {
-  //   if (this.verificarEmailForm.valid) {
-  //     const userData = this.verificarEmailForm.value;
-  //     this.generalService.verifyEmailUser(userData).subscribe(
-  //       (response) => {
-  //         console.log('Usuario verificado con éxito', response);
-  //         Swal.fire('Usuario Verificado con éxito', '', 'success');
-  //         this.router.navigate(['/safaTube/login']);
-  //       },
-  //       (error) => {
-  //         console.error('Error al verificar usuario', error);
-  //         Swal.fire('¡Error al verificar usuario!', '', 'error');
-  //       },
-  //       () => {
-  //         this.router.navigate(['/safaTube/login']);
-  //       }
-  //     );
-  //   }
-  // }
   token: string = '';
   usuario: any; // Asegúrate de especificar el tipo correcto si es posible
 
